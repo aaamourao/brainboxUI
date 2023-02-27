7dc6cf3a73e02aa0cabc964714f303d6cb63da0c
adriano mourao <mourao.aaa@gmail.com>
2023-02-26 23:23:05 -0300

    Add support for multiple bouding boxes

--- src/Canvas/Canvas.js
+++ src/Canvas/Canvas.js
@@ -21,9 +21,10 @@ class Canvas extends Component {
     this.canvasRef = createRef();
 
     this.handleChange = this.handleChange.bind(this);
-    //this.startDrawingRectangle = this.drawRectangle.bind(this);
+    this.startDrawingRectangle = this.startDrawingRectangle.bind(this);
     this.drawRectangle = this.drawRectangle.bind(this);
     this.stopDrawingRectangle = this.stopDrawingRectangle.bind(this);
+    this.getRectProps = this.getRectProps.bind(this);
   }
 
   componentDidMount() {
@@ -31,7 +32,7 @@ class Canvas extends Component {
     const context = canvas.getContext("2d");
     context.lineCap = "round";
     context.strokeStyle = "black";
-    context.lineWidth = 1;
+    context.lineWidth = 2;
     this.contextRef = context;
   }
 
@@ -61,6 +62,38 @@ class Canvas extends Component {
     event.preventDefault();
     event.stopPropagation();
 
+    const [rectWidth, rectHeight] = this.getRectProps(event);
+
+    this.contextRef.clearRect(0, 0, this.canvasRef.current.width, this.canvasRef.current.height);
+    this.contextRef.strokeRect(this.startX, this.startY, rectWidth, rectHeight);
+    this.state.boundingBoxes.map(bb => this.contextRef.strokeRect(bb.startX, bb.startY, bb.rectWidth, bb.rectHeight));
+  }
+
+
+  stopDrawingRectangle(event) {
+
+    if (!this.isDrawing) {
+      return;
+    }
+
+    const [rectWidth, rectHeight] = this.getRectProps(event);
+
+    let newBoundingBoxes = [...this.state.boundingBoxes];
+    
+    newBoundingBoxes.push({
+      startX: this.startX,
+      startY: this.startY,
+      rectWidth: rectWidth,
+      rectHeight: rectHeight,
+    });
+    
+    this.setState({
+      boundingBoxes: newBoundingBoxes
+    });
+    this.isDrawing = false;
+  }
+
+  getRectProps(event) {
     const canvasOffSet = this.canvasRef.current.getBoundingClientRect();
 
     const scaleX = this.canvasRef.current.width / canvasOffSet.width;
@@ -72,14 +105,7 @@ class Canvas extends Component {
     const rectWidth = newMouseX - this.startX;
     const rectHeight = newMouseY - this.startY;
 
-    this.contextRef.clearRect(0, 0, this.canvasRef.current.width, this.canvasRef.current.height);
-    this.contextRef.strokeRect(this.startX, this.startY, rectWidth, rectHeight);
-  }
-
-
-  stopDrawingRectangle() {
-    this.isDrawing = false;
-    console.log("drawed!!!")
+    return [rectWidth, rectHeight];
   }
 
   handleChange(event) {
